533c2f0ebfde84ed5a31ba73804017d6d32d4380
jppimentel <joao.pimentel@engenharia.ufjf.br>
2021-04-25 22:51:46 -0300

    Final lesson

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -20,7 +20,8 @@ class ConnectionsService {
 
     async create({socket_id, user_id, admin_id, id}: IConnectionCreate){
         
-        const connection = await this.connectionsRepository.findOne({socket_id, user_id, admin_id, id})
+        // const connection = await this.connectionsRepository.findOne({socket_id, user_id, admin_id, id})
+        const connection = await this.connectionsRepository.create({socket_id, user_id, admin_id, id});
 
         await this.connectionsRepository.save(connection);
 
@@ -33,6 +34,26 @@ class ConnectionsService {
         })
 
         return connection;
+    }
+
+    async findAllWithoutAdmin(){
+        const connections = await this.connectionsRepository.find({
+            where: { admin_id: null },
+            relations: ["user"]
+        });
+
+        return connections;
+    }
+
+    async findBySocketId(socket_id: string){
+        const connection = await this.connectionsRepository.findOne({
+            socket_id,
+        });
+        return connection;
+    }
+
+    async updateAdminID(user_id: string, admin_id: string){
+        await this.connectionsRepository.createQueryBuilder().update(Connection).set({admin_id}).where("user_id = :user_id", {user_id}).execute();
     }
 }
 
